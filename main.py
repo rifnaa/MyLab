35f41208ad1e82ff5c59db9954864081ace4f6d6
mohammad-ezest <mohammad.i@e-zest.com>
2024-09-04 17:29:16 +0100

    second commit

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
 
     data_list = [("Luka",  28),
                  ("Ram", 27),
-                 ("David", 58)]
+                 ("David", 58),
+                 ("Ronn", 23)]
 
     df = spark.createDataFrame(data_list).toDF("Name", "Age")
     df.show()
