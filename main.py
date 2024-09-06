98bf17f7d0c477cc4af95f5e890e88c9821de967
mohammad-Islam <rifnaarishan1520@gmail.com>
2024-09-06 12:43:33 +0100

    next

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     data_list = [("Luka",  28),
                  ("Ram", 27),
                  ("David", 58),
-                 ("Ronn", 23)]
+                 ("Ronn", 23),
+                 ("Fil", 59)]
 
     df = spark.createDataFrame(data_list).toDF("Name", "Age")
     df.show()
